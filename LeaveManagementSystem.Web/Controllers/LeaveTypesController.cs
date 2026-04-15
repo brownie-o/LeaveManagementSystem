172a0cedb49c4d976488b9f26b232eb75bd93821
160795-yu <fjuais08@fju.edu.tw>
2026-04-15 13:17:28 +0800

    [feat] Completed Leave Management Module

---
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -0,0 +1,167 @@
+﻿using Humanizer;
+using LeaveManagementSystem.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.ContentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        // dependency injection of the database context to access the database
+        private readonly ApplicationDbContext _context; // _context is the db connection
+
+        public LeaveTypesController(ApplicationDbContext context) 
+        {
+            _context = context; 
+        }
+
+        // GET: LeaveTypes
+        public async Task<IActionResult> Index()
+        {
+            // var data = SELECT * FROM LeaveTypes
+            // _context.LeaveTypes: connection to the database, and get the LeaveTypes table.
+            var data = await _context.LeaveTypes.ToListAsync();
+            return View(data);
+        }
+
+        // GET: LeaveTypes/Details/5
+        public async Task<IActionResult> Details(int? id) // [id] == asp-route-[id] in Index.cshtml
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Parameterization (User input is NOT directly concatenated into SQL strings. Instead, it is passed as a separate parameter)
+            // - key for preventing SQL injection (EF core translates LINQ query into SQL)
+            // Select * from LeaveTypes WHERE Id = [id]
+            var leaveType = await _context.LeaveTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaveType);
+        }
+
+        // GET: LeaveTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken] // protect against cross-site request forgery (CSRF) attacks
+        public async Task<IActionResult> Create([Bind("Id,Name,NumberOfDays")] LeaveType leaveType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(leaveType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaveType); // if the model state is not valid, return the view(Create.cshtml) with the current data(leaveType) to show validation errors
+        }
+
+        // GET: LeaveTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var leaveType = await _context.LeaveTypes.FindAsync(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+            return View(leaveType);
+        }
+
+        // POST: LeaveTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,NumberOfDays")] LeaveType leaveType)
+        {
+            if (id != leaveType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(leaveType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException) // handle concurrency issues (e.g., when multiple users try to edit the same record at the same time)
+                {
+                    if (!LeaveTypeExists(leaveType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(leaveType);
+        }
+
+        // GET: LeaveTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var leaveType = await _context.LeaveTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaveType);
+        }
+
+        // POST: LeaveTypes/Delete/5
+        [HttpPost]
+        // override the action name for the method (rename DeleteConfirmed to Delete)
+        [ActionName("Delete")] // to be called from the form in Delete.cshtml by Delete(asp-action="Delete"), to ensure the action name is same as the route
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var leaveType = await _context.LeaveTypes.FindAsync(id);
+            if (leaveType != null)
+            {
+                _context.LeaveTypes.Remove(leaveType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LeaveTypeExists(int id)
+        {
+            return _context.LeaveTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")] // [MaxLength(150)]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
